b2ee92c2e410f588b0ae89d88e85a0dfd9c64f6e
Collin Glass <collinglass@gmx.com>
2014-02-05 00:09:19 -0500

    Initial Commit

---
+++ src/Decoder.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+
+public class Decoder {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ServerSocket MyService = null;
+		Socket serviceSocket = null;
+		BufferedReader input;
+		DataOutputStream output;
+		String inputString;
+	    try {
+	       MyService = new ServerSocket(3000);
+	       serviceSocket = MyService.accept();
+	       System.out.println("Hello World");
+	       input = new BufferedReader(new InputStreamReader(serviceSocket.getInputStream()));
+	       output = new DataOutputStream(serviceSocket.getOutputStream());
+	       while (true) {
+	    	   inputString = input.readLine();
+				if ( inputString.equals("request-to-send") ) {
+					output.writeUTF("clear-to-send");
+					System.out.println(inputString);
+					break;
+				}
+	       }
+	       while (true) {
+	    	   inputString = input.readLine();
+	    	   System.out.println(inputString);
+	    	   break;
+	       }
+	       output.close();
+	       input.close();
+	       serviceSocket.close();
+	       MyService.close();
+	        }
+	        catch (IOException e) {
+	           System.out.println("Decoder Exception" + e);
+	        }
+	}
+
+}
---
+++ src/Encoder.java
@@ -0,0 +1,70 @@
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+public class Encoder {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Socket MyClient;
+		DataOutputStream output;
+		BufferedReader input;
+		String inputString;
+		String host = "";
+		
+		System.out.println("Please input binary string:");
+		String s = getInput();
+		
+		try {
+			host = args[0];
+			System.out.println(args[0]);
+			MyClient = new Socket(host, 3000);
+			input = new BufferedReader(new InputStreamReader(MyClient.getInputStream()));
+			output = new DataOutputStream(MyClient.getOutputStream());
+			
+			while (true) {
+				inputString = input.readLine();
+				if ( inputString.equals("clear-to-send") ) {
+					output.writeUTF(s);
+					break;
+				}
+			}
+				
+			output.close();
+	        input.close();
+	        MyClient.close();
+		}
+		catch(UnknownHostException e)
+	      {
+	         System.err.println("Cannot find host called: " + host);
+	         e.printStackTrace();
+	         System.exit(-1);
+	      }
+	      catch(IOException e)
+	      {
+	         System.err.println("Could not establish connection for " + host);
+	         e.printStackTrace();
+	         System.exit(-1);
+	      }
+	}
+	
+	private static String getInput() {
+		Scanner scan = new Scanner(System.in);
+		String result = "";
+		try {
+			int i = scan.nextInt();
+			while ( i <= 1 && i >= 0 ) {
+				result = result + i;
+				i = scan.nextInt();
+			}
+		} catch (Exception e) {
+			System.out.println("You entered an invalid input. You're only allowed 0 or 1 silly!");
+		}
+		return result;
+	}
+}
